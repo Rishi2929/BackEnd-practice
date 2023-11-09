33d65e723a3af985e63643d79ba0d2c46968f983
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-11-09 20:48:29 +0530

    Express.js

--- NodeJsBasics/index.js
+++ NodeJsBasics/index.js
@@ -1,40 +1,25 @@
-// const http = require("http");
-// const tech = require("./features");
+import express from 'express';
+import path from "path"
+const app = express();
 
-import http from "http";
-import tech, { GenerateNo, exp, lang } from "./features.js";
-import fs from "fs";
-import path from 'path';
-// import * as myObj from "./features.js";
-
-const home = fs.readFileSync("./index.html")
-
-console.log(tech);
-console.log(exp);
-console.log(lang);
-console.log(GenerateNo())
-console.log(path.dirname("/home/random/index.html"))
-
-// console.log(myObj)
-
-
-const server = http.createServer((req, res) => {
-    // res.end("<h1>noice</h1>");
-    if (req.url == "/about") {
-        res.end(`<h1>You are now redirected to About Page</h1><h2>${GenerateNo()}</h2>`)
-    }
-    else if (req.url == "/") {
-        // res.end("<h1>You are on Home Page</h1>")
-        res.end(home)
-    }
-    else if (req.url == "/contact") {
-        res.end("<h1>You are now redirected to Contact Page</h1>")
-    }
-    else {
-        res.end("<h1>Page not found</h1>")
-    }
+app.get("/", (req, res) => {
+    // res.sendStatus(404)
+    res.json({
+        Name: "Steve",
+        Profession: "Engineer",
+        Skills: []
+    })
 });
 
-server.listen(3000, () => {
-    console.log("Server is working");
-});
+app.get("/about", (req, res) => {
+    res.status(400).send("About Page not found")
+})
+
+app.get("/index", (req, res) => {
+    const pathlocation = path.resolve();
+    res.sendFile(path.join(pathlocation, "./index.html"))
+})
+
+app.listen(3000, () => {
+    console.log("Server is working")
+})
