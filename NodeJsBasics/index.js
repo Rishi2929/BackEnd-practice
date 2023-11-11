90614f3daf4655a89cf07cb0b12bfd840d3f877c
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-11-11 23:14:02 +0530

    Covered Ejs and now starting mongodb

--- NodeJsBasics/index.js
+++ NodeJsBasics/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
-// import path from "path"
+import path from "path";
+
 const app = express();
+const users = [];
+
+
+//Middleware
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");//Setting up view engine
 
@@ -9,14 +16,30 @@ app.get("/", (req, res) => {
 
 });
 
+app.get("/add", (req, res) => {
+    res.send("nice")
+
+});
+
 app.get("/about", (req, res) => {
     res.status(400).send("About Page not found")
 })
 
-app.get("/index", (req, res) => {
-    const pathlocation = path.resolve();
-    res.sendFile(path.join(pathlocation, "./index.html"))
+app.get("/success", (req, res) => {
+    res.render("success");
 })
+app.post("/", (req, res) => {
+    users.push({ username: req.body.name, email: req.body.email });
+    res.redirect("/success");
+});
+
+
+app.get("/users", (req, res) => {
+    res.json({
+        users,
+    });
+})
+
 
 app.listen(3000, () => {
     console.log("Server is working")
