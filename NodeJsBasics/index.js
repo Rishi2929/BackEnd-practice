7e0ad5e80b21c8e627fd68641fa3c916b65be4e9
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-11-12 21:12:50 +0530

    Mongodb

--- NodeJsBasics/index.js
+++ NodeJsBasics/index.js
@@ -1,46 +1,48 @@
 import express from 'express';
 import path from "path";
+import mongoose from 'mongoose';
+
+await mongoose.connect('mongodb://localhost:27017/backend', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("Database connected"))
+    .catch(error => console.error("Database not connected", error));
+
+
+
+
+const msgSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const Message = mongoose.model("Message", msgSchema);
 
 const app = express();
 const users = [];
 
-
-//Middleware
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");//Setting up view engine
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index", { name: "Rishi" });
-
 });
 
-app.get("/add", (req, res) => {
-    res.send("nice")
-
-});
-
-app.get("/about", (req, res) => {
-    res.status(400).send("About Page not found")
-})
-
-app.get("/success", (req, res) => {
-    res.render("success");
-})
-app.post("/", (req, res) => {
-    users.push({ username: req.body.name, email: req.body.email });
-    res.redirect("/success");
+app.get("/add", async (req, res) => {
+    try {
+        const result = await Message.create({ name: "Arvind", email: "Chirag@gmail.com" });
+        console.log("Data inserted:", result);
+        res.send("nice");
+    } catch (error) {
+        console.error("Error adding message:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 
-app.get("/users", (req, res) => {
-    res.json({
-        users,
-    });
-})
-
+// ... (rest of your routes)
 
 app.listen(3000, () => {
-    console.log("Server is working")
-})
+    console.log("Server is working");
+});
