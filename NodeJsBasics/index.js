f5728d7775e1cc172bd38a50aa1fb31f1356ed91
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2023-11-15 16:29:47 +0530

    learning authentication

--- NodeJsBasics/index.js
+++ NodeJsBasics/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from "path";
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
 
 mongoose.connect('mongodb://localhost:27017/', {
     dbName: "backend",
@@ -17,13 +18,37 @@ const app = express();
 //Middleware
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.set("view engine", "ejs");//Setting up view engine
 
 app.get("/", (req, res) => {
-    res.render("index", { name: "Rishi" });
+    // res.cookie("token", "iamin");
+    const { token } = req.cookies;
+    if (token) {
+        res.render("logout",);
+    } else {
 
+        res.render("login",);
+
+    }
 });
+
+app.post("/login", (req, res) => {
+    res.cookie("token", "iamin", {
+        httpOnly: true,
+    });
+    res.redirect("/")
+})
+
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", null, {
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    })
+    res.redirect("/")
+})
 
 
 
